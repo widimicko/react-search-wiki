aea2d275302a0a6a2cad26750bf6bf58c07785c2
Micko Widi <widimicko85@gmail.com>
2022-03-01 10:13:54 +0700

    add tailwind

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import './style.css'
-
 const Input = ({placeholder, ...rest}) => <input className='input-field' placeholder={placeholder} {...rest}/>
 
 export default Input;
